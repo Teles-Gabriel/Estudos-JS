fef5b16356c540e44c0ee3b2bd8db19e31cd1445
Teles-Gabriel <gabrielteles0609@gmail.com>
2023-03-09 04:46:50 -0300

    Condições com js

---
+++ Exercicio4/exercicio.js
@@ -0,0 +1,12 @@
+console.log('Gabriel');
+
+var vel = 60.5
+
+console.log(`a velocidade do carro é ${vel}`);
+
+if(vel > 60) {
+    console.log(`Multa, sua velocidade é maior que permitida`);
+    console.log(vel);
+} else {
+    console.log('Dirija com cuidado');
+}

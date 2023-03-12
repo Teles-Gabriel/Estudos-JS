1f34b79994ba6c767c01b6549970fcebe03f38bf
Teles-Gabriel <gabrielteles0609@gmail.com>
2023-03-11 21:31:00 -0300

    Laços de repetição

---
+++ Exercicio6/atividade1.js
@@ -0,0 +1,22 @@
+var c = 1
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while(c<=6)
+
+
+
+var d = 1
+while(d <= 10){
+    console.log(`passo ${d}`)
+    d++
+}
+
+
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
